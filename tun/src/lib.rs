c2e97720ad553757ddd75079d00599e9e4e4fc05
Lee Smet <lee.smet@hotmail.com>
2024-04-25 16:39:18 +0200

    Add MTU to config builder
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- tun/src/lib.rs
+++ tun/src/lib.rs
@@ -6,8 +6,10 @@ use std::net::Ipv6Addr;
 pub struct ConfigBuilder {
     /// Name for the tun interface. Platform restrictions apply.
     name: Option<String>,
-    /// IPv6 address to assign to the tun interface, as well as the
+    /// IPv6 address to assign to the tun interface, as well as the size of the subnet mask.
     ip6: Option<(Ipv6Addr, u8)>,
+    /// Mtu to set on the created interface.
+    mtu: Option<u32>,
 }
 
 impl ConfigBuilder {
@@ -16,6 +18,7 @@ impl ConfigBuilder {
         ConfigBuilder {
             name: None,
             ip6: None,
+            mtu: None,
         }
     }
 
@@ -29,6 +32,12 @@ impl ConfigBuilder {
     /// of the mask.
     pub fn ip6(mut self, ip6: Ipv6Addr, mask_size: u8) -> Self {
         self.ip6 = Some((ip6, mask_size));
+        self
+    }
+
+    /// Set the MTU to be used by the created interface.
+    pub fn mtu(mut self, mtu: u32) -> Self {
+        self.mtu = Some(mtu);
         self
     }
 }
